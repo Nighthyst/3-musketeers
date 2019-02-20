992866b035b638eb8f5c99ad2bfc3bf46b3c42cd
Nicolas BAOUAYA-MOULOMBA <nicolas.baouaya-moulomba@devinci.fr>
2019-02-20 23:16:47 +0100

    4 tests done for starwars

--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -7,20 +7,33 @@ describe('starwars-names', () => {
       expect(starWars.all).toHaveLength(starWarsNames.length)
     });
     test('should be an array of strings', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      const actual = true;
+      starWars.all.forEach(function(element){
+        if(typeof(element) != "string")
+        {
+          actual = false;
+        }
+      });
+      expect(actual).toEqual(true);
+      //throw new Error('test not yet defined... remove the throw and write your test here');
     });
 
     test('should contain `Luke Skywalker`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).toContain("Luke Skywalker");
+      //throw new Error('test not yet defined... remove the throw and write your test here');
     });
 
     test('should not contain `Ben Quadinaros`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      const actual = starWars.all.includes(`Ben Quadinaros`)
+      const expectation = false;
+      expect(actual).toEqual(expectation);
+      //throw new Error('test not yet defined... remove the throw and write your test here');
     });
   });
 
   describe('random', () => {
     test('should return a random item from the starWars.all', () => {
+
       throw new Error('test not yet defined... remove the throw and write your test here');
     });
 
