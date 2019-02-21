b8b16a8bbc43baa1191b7e09209a13f83db96c61
Nicolas BAOUAYA-MOULOMBA <nicolas.baouaya-moulomba@devinci.fr>
2019-02-21 12:43:29 +0100

    Everything is done

--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -33,12 +33,31 @@ describe('starwars-names', () => {
 
   describe('random', () => {
     test('should return a random item from the starWars.all', () => {
+      var actual = false;
+      var expectation = true;
 
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      if(starWars.all.includes(starWars.random(1)[0]))
+      {
+        actual = true;
+      }
+
+      expect(actual).toEqual(expectation);
+      //throw new Error('test not yet defined... remove the throw and write your test here');
     });
 
     test('should return an array of random items if passed a number', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      var number = Math.floor((Math.random() * starWars.all.length) + 1);
+      var random_items = starWars.random(number);
+      var expectation = true;
+      var actual = true;
+      random_items.forEach(function(element){
+        if(starWars.all.includes(element) === false)
+        {
+          actual = false;
+        }
+      });
+      expect(actual).toEqual(expectation);
+      //throw new Error('test not yet defined... remove the throw and write your test here');
     });
   });
 });
